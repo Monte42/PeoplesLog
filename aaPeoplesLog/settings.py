30bfa5bcc21d47e9f3098808d3fbf3abb1048732
Monte DuMond <gdumond42@gmail.com>
2020-08-04 19:30:55 -0400

    added aws s3

--- aaPeoplesLog/settings.py
+++ aaPeoplesLog/settings.py
@@ -157,7 +157,7 @@ AWS_STORAGE_BUCKET_NAME = 'peopleslog1-bucket'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 # <?xml version="1.0" encoding="UTF-8"?>
