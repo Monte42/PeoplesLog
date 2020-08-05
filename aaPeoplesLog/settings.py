6ef6139e55523e5c6b22c16e3cfd22ac0687c788
Monte DuMond <gdumond42@gmail.com>
2020-08-04 20:22:13 -0400

    env var

--- aaPeoplesLog/settings.py
+++ aaPeoplesLog/settings.py
@@ -154,7 +154,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4' # This tells crispt to use bootstrap4
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'peopleslog1-bucket'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
