89693f4ed9a223e606659919fd586637ce0a3257
Monte DuMond <gdumond42@gmail.com>
2020-09-01 07:29:28 -0400

    Proc and stuff

--- aaPeoplesLog/asgi.py
+++ aaPeoplesLog/asgi.py
@@ -1,16 +1,6 @@
-"""
-ASGI config for aaPeoplesLog project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
-
 import os
-import django
-from channels.routing import get_default_application
+from channels.asgi import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aaPeoplesLog.settings")
-django.setup()
-application = get_default_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
+
+channel_layer = get_channel_layer()
